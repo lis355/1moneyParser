7dcc7c243c9354b37367e9a510ffdfadf83ad39f
Ivan Lartsov <lartsov@gmail.com>
2024-03-08 14:32:52 +0300

    upd: moved text to README

--- start.js
+++ start.js
@@ -3,28 +3,6 @@ import ndapp from "ndapp";
 
 dotenv();
 
-/**
- * Конвертирует базу данных бэкапов приложения 1Money
- * https://play.google.com/store/apps/details?id=org.pixelrush.moneyiq
- *
- * Для извлечения данных нужно взять бэкап из сотового, с помощью DB Browser (SQLite) (https://sqlitebrowser.org/)
- * сконвертировать бэкап в папку с даннымы в json формате
- *
- * Указать в .env.local
- * DATABASE_DIRECTORY=путь до папки с даннымы в json формате
- * FROM_DATE=дата начала периода (формат ISO 8601, пример 2024-03-08T10:47:11.554Z)
- * TO_DATE=дата конца периода (формат ISO 8601)
- * OUTPUT_PATH=путь до файла для выгрузки
- * OUTPUT_FORMAT=CSV или JSONLINES
- *
- * Пример
- * DATABASE_DIRECTORY=C:/Users/LIS355/Desktop/b/json
- * FROM_DATE="2024-02-01T10:00:00.000Z"
- * TO_DATE="2024-03-01T10:00:00.000Z"
- * OUTPUT_PATH=C:/Users/LIS355/Desktop/b/json/out.csv
- * OUTPUT_FORMAT=CSV
- */
-
 const CATEGORY_TRANSLATE = {
 	"Transport": "Транспорт",
 	"Groceries": "Продукты",
